a71f4c42538143e39cb3d54e9b94a42ca791aac0
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2024-01-22 18:29:13 -0500

    Update loss.py

--- model/loss.py
+++ model/loss.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 
-def loss(y, t, y0_pred, y1_pred, t_pred):
+def model_loss(y, t, y0_pred, y1_pred, t_pred):
     loss_t = F.binary_cross_entropy(t_pred, t)
     loss_y = torch.sum((1. - t) * torch.square(y - y0_pred)) + torch.sum(t * torch.square(y - y1_pred))
     return loss_y + loss_t
