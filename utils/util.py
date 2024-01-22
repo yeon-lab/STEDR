782afb9f3da3417927773aeae7d9a79270e803ed
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2024-01-22 18:33:24 -0500

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -3,7 +3,17 @@ from pathlib import Path
 from collections import OrderedDict
 from itertools import repeat
 import pandas as pd
+import numpy as np
 
+def padding(seqs, input_dim, maxlen):
+    lengths = np.array([len(seq) for seq in seqs]).astype("int32")
+    n_samples = len(seqs)
+    x = np.zeros([n_samples, maxlen, input_dim]).astype(np.float32)
+    for idx, seq in enumerate(seqs):
+        for xvec, subseq in zip(x[idx, :, :], seq):
+            xvec[subseq] = 1.
+    return x, lengths
+    
 def ensure_dir(dirname):
     dirname = Path(dirname)
     if not dirname.is_dir():
