a2351cd6e60a09302a02db7810daf4074654a6cf
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2024-01-22 18:21:41 -0500

    Update trainer.py

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,8 +1,12 @@
 import numpy as np
 import torch
-from trainer.base_trainer import BaseTrainer
+from base_trainer import BaseTrainer
 from utils import inf_loop, MetricTracker
-from model.metric import compute_variances
+from utils import padding
+import torch.nn as nn
+import pandas as pd
+from metric import compute_variances
+import os
 
 class Trainer(BaseTrainer):
     """
@@ -17,159 +21,213 @@ class Trainer(BaseTrainer):
                       test_set):
         super().__init__(model, metric_ftns, optimizer, config)
         self.config = config
-        self.batch_size = config['data_loader']['batch_size']
-        self.d_type = self.config['data_loader']['data']
+        self.batch_size = config["batch_size"]
+        self.maxlen = config['maxlen']
+        self.is_EHR = config['is_EHR']
+
         self.n_clusters = self.model.n_clusters
         self.input_dim = self.model.input_dim
-        
+
         self.train_set = train_set
         self.valid_set = valid_set
         self.test_set = test_set
+        self.n_train = len(train_set['X'])
         
-        self.train_n_batches = int(np.ceil(float(config["data_loader"]["n_train"]) / float(self.batch_size)))
-        self.valid_n_batches = int(np.ceil(float(config["data_loader"]["n_valid"]) / float(self.batch_size)))
-        self.test_n_batches = int(np.ceil(float(config["data_loader"]["n_test"]) / float(self.batch_size)))
+        self.train_n_batches = int(np.ceil(float(self.n_train) / float(self.batch_size)))
+        self.valid_n_batches = int(np.ceil(float(len(valid_set['X'])) / float(self.batch_size)))
+        self.test_n_batches = int(np.ceil(float(len(test_set['X'])) / float(self.batch_size)))
 
         self.do_validation = self.valid_set is not None
         self.lr_scheduler = optimizer
-        self.log_step = 4  # reduce this if you want more logs
+        self.log_step = 32  # reduce this if you want more logs
 
         self.metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns])
 
-    def _train_epoch(self, epoch):        
-        self.metrics.reset()                
-        y0_outs, y1_outs, t_outs = torch.tensor([]).to(self.device), torch.tensor([]).to(self.device), torch.tensor([]).to(self.device)
-        y_trgs, t_trgs, te_trgs = torch.tensor([]).to(self.device), torch.tensor([]).to(dtype=torch.int64).to(self.device), torch.tensor([]).to(self.device)
-        assigned_clusters = torch.tensor([]).to(self.device)
-        for idx in range(self.train_n_batches):
-            self.model.train()
-            x = torch.FloatTensor(
-                    self.train_set['X'][idx*self.batch_size:(idx+1)*self.batch_size]
-                ).to(self.device)
+
+        
+    def _train_epoch(self, epoch):
+        self.model.train()
+        self.metrics.reset()        
+        y0_outs, y1_outs, t_outs = np.array([]), np.array([]), np.array([])
+        y_trgs, t_trgs, te_trgs = np.array([]), np.array([]), np.array([])
+        assigned_clusters = np.array([])
+        x_lengths = None
+        for index in range(self.train_n_batches):
+            x = self.train_set['X'][index*self.batch_size:(index+1)*self.batch_size]
+            if self.is_EHR:
+                x, x_lengths = padding(x, self.input_dim, self.maxlen)
+            else:
+                te = torch.Tensor(
+                        self.train_set['TE'][index*self.batch_size:(index+1)*self.batch_size]                    
+                    ).to(self.device)
+            x = torch.from_numpy(x).float().to(self.device)
             t = torch.Tensor(
-                    self.train_set['T'][idx*self.batch_size:(idx+1)*self.batch_size]                    
+                    self.train_set['T'][index*self.batch_size:(index+1)*self.batch_size]                    
                 ).to(self.device)            
             y = torch.Tensor(
-                    self.train_set['Y'][idx*self.batch_size:(idx+1)*self.batch_size]                    
-                ).to(self.device)
-            te = torch.Tensor(
-                    self.train_set['TE'][idx*self.batch_size:(idx+1)*self.batch_size]                    
+                    self.train_set['Y'][index*self.batch_size:(index+1)*self.batch_size]                    
                 ).to(self.device)
 
             self.optimizer.zero_grad()
-            loss, y0_pred, y1_pred, t_pred, clusters = self.model.predict(x, t, y)
+            loss, y0_pred, y1_pred, t_pred, clusters, _ = self.model.predict(x, t, y, x_lengths)
             loss.backward(retain_graph=True)
             self.optimizer.step()
             self.metrics.update('loss', loss.item())
             
-            if idx % self.log_step == 0:
-                self.logger.debug('Train Epoch: {} {} Loss: {:.6f} '.format(
+            if index % self.log_step == 0:
+                print('Train Epoch: {} {} Loss: {:.6f} '.format(
                     epoch,
-                    self._progress(idx),
+                    self._progress(index),
                     loss.item(),
                 ))
                 
-            clusters = torch.argmax(clusters, dim=1)
-            assigned_clusters = torch.cat([assigned_clusters, clusters], 0)
+            assigned_clusters = np.append(assigned_clusters, clusters.cpu().numpy())
             
-            y0_outs, y1_outs = torch.cat([y0_outs, y0_pred]), torch.cat([y1_outs, y1_pred])
-            y_trgs, t_trgs = torch.cat([y_trgs, y]), torch.cat([t_trgs, t])
-            te_trgs = torch.cat([te_trgs, te])
+            y0_outs = np.append(y0_outs, y0_pred.detach().cpu().numpy())
+            y1_outs = np.append(y1_outs, y1_pred.detach().cpu().numpy())
+            if self.is_EHR:
+                t_outs = np.append(t_outs, t_pred.detach().cpu().numpy())
+            else:
+                te_trgs = np.append(te_trgs, te.cpu().numpy())
+            y_trgs = np.append(y_trgs, y.cpu().numpy())
+            t_trgs = np.append(t_trgs, t.cpu().numpy())
             
-            with torch.no_grad():
-                self.model.eval()
-                self.model.update_centers(x)
 
-        for met in self.metric_ftns:
-            self.metrics.update(met.__name__, met(t_trgs, y_trgs, te_trgs, y0_outs, y1_outs))
+        for met in self.metric_ftns: 
+            if self.is_EHR:
+                self.metrics.update(met.__name__, met(t_trgs, y_trgs, y0_outs, y1_outs, t_outs))
+            else:
+                self.metrics.update(met.__name__, met(t_trgs, y_trgs, te_trgs, y0_outs, y1_outs))
                 
         log = self.metrics.result() 
-        within_var, across_var = compute_variances(te_trgs, assigned_clusters, self.n_clusters)
-        clusters, counts = assigned_clusters.unique(return_counts=True, sorted=True)
+        
+        if self.is_EHR:
+            TE = y1_outs - y0_outs
+        else:
+            TE = te_trgs
+
+        within_var, across_var = compute_variances(TE, assigned_clusters, self.n_clusters)
+        clusters, counts = np.unique(assigned_clusters, return_counts=True)
         
         log.update({'clusters': clusters.tolist()})
         log.update({'counts': counts.tolist()})
         log.update({'within_var':within_var})
         log.update({'across_var':across_var})  
         
-        if self.do_validation:
-            val_log = self._infer(self.valid_set, self.valid_n_batches)
-            log.update(**{'val_' + k: v for k, v in val_log.items()})      
-        return log
-
-    def _infer(self, data_set, n_batches):
-        self.model.eval()
-        self.metrics.reset()
-        with torch.no_grad():
-            y0_outs, y1_outs, t_outs = torch.tensor([]).to(self.device), torch.tensor([]).to(self.device), torch.tensor([]).to(self.device)
-            y_trgs, t_trgs, te_trgs = torch.tensor([]).to(self.device), torch.tensor([]).to(dtype=torch.int64).to(self.device), torch.tensor([]).to(self.device)
-            assigned_clusters = torch.tensor([]).to(self.device)
-            for idx in range(n_batches):
-                x = torch.FloatTensor(
-                        data_set['X'][idx*self.batch_size:(idx+1)*self.batch_size]                    
-                    ).to(self.device)   
-                t = torch.Tensor(
-                        data_set['T'][idx*self.batch_size:(idx+1)*self.batch_size]                    
-                    ).to(self.device)            
-                y = torch.Tensor(
-                        data_set['Y'][idx*self.batch_size:(idx+1)*self.batch_size]                    
-                    ).to(self.device)
-                te = torch.Tensor(
-                        data_set['TE'][idx*self.batch_size:(idx+1)*self.batch_size]                    
-                    ).to(self.device)
-                
-                loss, y0_pred, y1_pred, t_pred, clusters = self.model.predict(x, t, y)
-                self.metrics.update('loss', loss.item())
-                
-                clusters = torch.argmax(clusters, dim=1)
-                assigned_clusters = torch.cat([assigned_clusters, clusters])
-                y0_outs, y1_outs = torch.cat([y0_outs, y0_pred]), torch.cat([y1_outs, y1_pred])
-                y_trgs, t_trgs = torch.cat([y_trgs, y]), torch.cat([t_trgs, t])
-                te_trgs = torch.cat([te_trgs, te])
-                
-                
-        for met in self.metric_ftns:
-            self.metrics.update(met.__name__, met(t_trgs, y_trgs, te_trgs, y0_outs, y1_outs))
-                
-        log = self.metrics.result()
+        for c in clusters.tolist():
+            sub_te = TE[assigned_clusters==c]
+            log.update({'TE avg. for group {}'.format(c): np.mean(sub_te)})
+            log.update({'TE std. for group {}'.format(c): np.std(sub_te)})
         
-        within_var, across_var = compute_variances(te_trgs, assigned_clusters, self.n_clusters)
-
-        clusters, counts = assigned_clusters.unique(return_counts=True, sorted=True)
-        log.update({'clusters': clusters.tolist()})
-        log.update({'counts': counts.tolist()})
-        log.update({'across_var':across_var})
-        log.update({'within_var':within_var})
+        
+        if self.do_validation:
+            valid_results = self._infer(self.valid_set, self.valid_n_batches)
+            log.update(**{'val_' + k: v for k, v in valid_results['log'].items()})
             
         return log
+
+        
+    def _infer(self, data_set, n_batches, phase=False):
+        self.metrics.reset()      
+        self.model.train()
+        y0_outs, y1_outs, t_outs = np.array([]), np.array([]), np.array([])
+        y_trgs, t_trgs, te_trgs = np.array([]), np.array([]), np.array([])
+        assigned_clusters, all_attentions = np.array([]), np.empty((0, self.config['input_dim']))
+        x_lengths = None
+        
+        for index in range(n_batches):
+            x = self.train_set['X'][index*self.batch_size:(index+1)*self.batch_size]
+            if self.is_EHR:
+                x, x_lengths = padding(x, self.input_dim, self.maxlen)
+            else:
+                te = torch.Tensor(
+                        self.train_set['TE'][index*self.batch_size:(index+1)*self.batch_size]                    
+                    ).to(self.device)
+            x = torch.from_numpy(x).float().to(self.device)
+            t = torch.Tensor(
+                    self.train_set['T'][index*self.batch_size:(index+1)*self.batch_size]                    
+                ).to(self.device)            
+            y = torch.Tensor(
+                    self.train_set['Y'][index*self.batch_size:(index+1)*self.batch_size]                    
+                ).to(self.device)
+            
+            loss, y0_pred, y1_pred, t_pred, clusters, attentions = self.model.predict(x, t, y, x_lengths)
+            self.metrics.update('loss', loss.item())
+
+            assigned_clusters = np.append(assigned_clusters, clusters.cpu().numpy())
+            all_attentions = np.vstack([all_attentions, attentions.detach().cpu().numpy()])
+            
+            y0_outs = np.append(y0_outs, y0_pred.detach().cpu().numpy())
+            y1_outs = np.append(y1_outs, y1_pred.detach().cpu().numpy())
+            if self.is_EHR:
+                t_outs = np.append(t_outs, t_pred.detach().cpu().numpy())
+            else:
+                te_trgs = np.append(te_trgs, te.cpu().numpy())
+            y_trgs = np.append(y_trgs, y.cpu().numpy())
+            t_trgs = np.append(t_trgs, t.cpu().numpy())
+            
+        for met in self.metric_ftns: 
+            if self.is_EHR:
+                self.metrics.update(met.__name__, met(t_trgs, y_trgs, y0_outs, y1_outs, t_outs))
+            else:
+                self.metrics.update(met.__name__, met(t_trgs, y_trgs, te_trgs, y0_outs, y1_outs))
+                
+        log = self.metrics.result() 
+        
+        if self.is_EHR:
+            TE = y1_outs - y0_outs
+        else:
+            TE = te_trgs
+            
+        log.update({'avg_TE': np.mean(TE)})
+
+        within_var, across_var = compute_variances(TE, assigned_clusters, self.n_clusters)
+        clusters, counts = np.unique(assigned_clusters, return_counts=True)
+        
+        log.update({'clusters': clusters.tolist()})
+        log.update({'counts': counts.tolist()})
+        log.update({'within_var':within_var})
+        log.update({'across_var':across_var})  
+        
+        for c in clusters.tolist():
+            sub_te = TE[assigned_clusters==c]
+            log.update({'TE avg. for group {}'.format(c): np.mean(sub_te)})
+            log.update({'TE std. for group {}'.format(c): np.std(sub_te)})
+            
+        results = {
+            'log': log,
+            'TE': TE,
+            'Y': y_trgs,
+            'T': t_trgs,
+            'assigned_clusters': assigned_clusters,
+            'all_attentions': all_attentions
+        }
+        return results
         
 
     def _test_epoch(self):
-        PATH = str(self.checkpoint_dir / 'model_best.pth')
-        self.model.load_state_dict(torch.load(PATH)['state_dict'])
-        self.model.eval()
-        
-        log = {}        
-        train_log = self._infer(self.train_set, self.train_n_batches)
-        valid_log = self._infer(self.valid_set, self.valid_n_batches)
-        test_log = self._infer(self.test_set, self.test_n_batches)
-        log.update(**{'train_' + k: v for k, v in train_log.items()})
-        log.update(**{'val_' + k: v for k, v in valid_log.items()})
-        log.update(**{'test_' + k: v for k, v in test_log.items()})
-                        
-        self.logger.info('='*100)
-        self.logger.info('Inference is completed')
-        self.logger.info('-'*100)
-        for key, value in log.items():
-            self.logger.info('    {:20s}: {}'.format(str(key), value))  
-        self.logger.info('='*100)
 
-        return log
+        log = {}        
+        train_results = self._infer(self.train_set, self.train_n_batches, phase='train')
+        valid_results = self._infer(self.valid_set, self.valid_n_batches, phase='valid')
+        test_results = self._infer(self.test_set, self.test_n_batches, phase='test')
+        
+
+        log.update(**{'train_' + k: v for k, v in train_results['log'].items()})
+        log.update(**{'val_' + k: v for k, v in valid_results['log'].items()})
+        log.update(**{'test_' + k: v for k, v in test_results['log'].items()})
+                        
+        print('='*100)
+        print('Inference is completed')
+        print('-'*100)
+        for key, value in log.items():
+            print('    {:20s}: {}'.format(str(key), value))  
+        print('='*100)
+        return train_results, valid_results, test_results
         
     def _progress(self, batch_idx):
         base = '[{}/{} ({:.0f}%)]'
         current = batch_idx * self.batch_size
-        total = self.config['data_loader']['n_train']
-        return base.format(current, total, 100.0 * current / total)
+        return base.format(current, self.n_train, 100.0 * current / self.n_train)
         
         
